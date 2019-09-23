6c334e8629a547d5501f61100802e82c4a3bbd7b
Ben Kreeger <ben@kree.gr>
2019-09-23 17:13:43 -0500

    Initial Commit

---
+++ SwiftSoupTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftSoupTest
+//
+//  Created by Ben Kreeger on 9/23/19.
+//  Copyright © 2019 Ben Kreeger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
